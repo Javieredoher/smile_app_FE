8421d2ba01105314cdff7eabbcedae5e70f1e546
PatriciaChaH <patriciachavezh@gmail.com>
2022-08-11 00:42:23 -0500

    Input formulario GeneralInfo

--- src/components/newPatientForms/GeneralInfo.jsx
+++ src/components/newPatientForms/GeneralInfo.jsx
@@ -2,7 +2,8 @@ import React from "react"
 import style from "../newPatientForms/general.module.css"
 import Title from "../newPatientForms/Title"
 import Input from "../newPatientForms/InputsForm"
-import TitleFields from "./TitleFields"
+import Fields from "./Fields"
+
 import newPatient from "../../assets/images/newPatient.png"
 
 
@@ -16,12 +17,55 @@ function GeneralInfo() {
     <div className={style.DivSubtitulo}>
         <h3>I. INFORMACIÓN GENERAL</h3>
     </div>   
-        <div className={style.DivInputs}>
-          <TitleFields value='ALERTA MÉDICA'/>
-          <div className={style.Input}> 
+        <div className={style.DivInputAlert}>
+          <Fields 
+          className={style.TitleAlert}
+          value='ALERTA MÉDICA'
+          />
+          <Input
+          className={style.InputAlert}
+          name="Alert"
+          placeholder=" Describa afecciones importantes del paciente"         
+          />
+          </div>
+            <div className={style.DivInputLine1}>
+            <Fields
+            className={style.TitleFirstSurname}
+            value="1er Apellido"
+            />
+            <Input
+            className={style.InputFirstSurname}
+            name="FirstSurName"
+            placeholder=" 1er Apellido"
+            />
+            <Fields
+            className={style.TitleSecondSurname}
+            value="2do Apellido"
+            />
+            <Input
+            className={style.InputSecondSurname}
+            name="SecondSurname"
+            placeholder=  " 2do Apellido"
+            />
+            <Fields
+            className={style.TitleFirstName}
+            value="1er Nombre"
+            />
+            <Input
+            className={style.InputFirstName}
+            name="FirstName"
+            placeholder=" 1er Nombre"
+            />
+            <Fields
+            className={style.TitleSecondName}
+            value="2do Nombre"
+            />
+            <Input
+            className={style.InputSecondName}
+            name="SecondName"
+            placeholder=" 2do Nombre"
+          />
         </div>
-    </div>
-
 
 
   </div>
--- src/components/newPatientForms/InputsForm.jsx
+++ src/components/newPatientForms/InputsForm.jsx
@@ -4,11 +4,11 @@ import style from '../newPatientForms/general.module.css'
 //export default function Input({value = '', type = 'text', placeholder = '', name = '', label = ''}) {
 //Se retira el atributo value, se debe reinstaurar cuando se haga la funcionalidad
 
-export default function Input({type = 'text', placeholder = '', name = '', label = '', titulo = ''}) {
+export default function Input({className='', type = 'text', placeholder = '', name = '  ', label = '', titulo = ''}) {
   return (
     <>
-        <Input
-        className={style.Input}
+        <input
+        className={className}
         type={type}
         placeholder={placeholder}
         //value={value}
