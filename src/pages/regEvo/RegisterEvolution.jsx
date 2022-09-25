d1e00f371010fe2b87383bcfb6d457dc97ab4fc1
nath4c <nath.2xac@gmail.com>
2022-09-25 09:11:39 -0500

    Creating register evolution module

--- src/pages/regEvo/RegisterEvolution.jsx
+++ src/pages/regEvo/RegisterEvolution.jsx
@@ -4,17 +4,85 @@ import style from './reg.evolution.module.css'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import Title from '../../components/Home/Title'
+import Fields from '../../components/newPatientForms/Fields'
+import Input from '../../components/newPatientForms/InputsForm'
+import Button from '../../components/button/Button'
 
 export const RegisterEvolution = () => {
-    return (
+return (
         <div className={style.REcontent}>
             <div className={style.Header}>
-            <Header/>
+                <Header/>
                 </div>
-            <div className={style.Title}>
+            <div className={style.DivTitle}>
                 <Title value='REGISTRAR EVOLUCIÓN'/>
                 </div>
-            <Footer/>
+            <div className={style.DivSubtitulo}>
+                <h3>DATOS PERSONALES</h3>
+                </div>
+                <div className={style.DivData}>
+                    <div className={style.Nombre}>
+                        <Fields 
+                        className={style.Label}
+                        value="Nombre"
+                        />
+                        <Input className={style.Input}/>
+                    </div>
+                    <div className={style.Nombre}>
+                        <Fields 
+                        className={style.Label}
+                        value="Apellido"
+                        />
+                        <Input className={style.Input}/>
+                    </div>
+                    <div className={style.Nombre}>
+                        <Fields 
+                        className={style.Label}
+                        value="N° Documento"
+                        />
+                        <Input className={style.InputInt}/>
+                    </div>
+                    <div className={style.Nombre}>
+                        <Fields 
+                        className={style.Label}
+                        value="Edad"
+                        />
+                        <Input className={style.InputInt}/>
+                    </div>
+                    <div className={style.Nombre}>
+                        <Fields 
+                        className={style.Label}
+                        value="Celular"
+                        />
+                        <Input className={style.InputInt}/>
+                    </div>
+                </div>
+            <div className={style.DivSubtitulo}>
+            <h3>EVOLUCIÓN</h3>
+                </div>
+                <div className={style.DivEvoInput}>
+                    <textarea
+                    className={style.EvoInput}
+                    name="Evolution Input"
+                    placeholder=  "Describa nuevos avances en el proceso clínico del paciente"
+                    />
+                </div>
+                <div className={style.DivButton}>
+                    <Button value='Adjuntar archivos'/>
+                </div>
+            <div className={style.DivSubtitulo}>
+                <h3>PROCEDIMIENTO (CUPS)</h3>
+                </div>
+                <div className={style.DivEvoInput}>
+                    <textarea
+                    className={style.EvoInput}
+                    name="Evolution Input"
+                    placeholder=  "Describa nuevos avances en el proceso clínico del paciente"
+                    />
+                </div>
+            <div className={style.Footer}>
+                <Footer/>
+                </div>
         </div>
     )
 }
